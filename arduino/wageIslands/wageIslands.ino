167e01201be936214c05fffaf73ba7a8afc5c7c0
Ekene Ijeoma <hello@ekeneijeoma.com>
2015-08-26 09:34:39 -0400

    temporarily removed auto mode cleaned up code

--- arduino/wageIslands/wageIslands.ino
+++ arduino/wageIslands/wageIslands.ino
@@ -3,28 +3,28 @@
 // FOR USE ON ARDUINO UNO R3
 
 /* Connections
- *  D7 to pushbutton with a pulldown resistor, this controls the movement of the motors 
+ *  D7 to pushbutton with a pulldown resistor, this controls the movement of the motors
  *  A4 to SDA, A5 to SDL of an I2C LCD
  */
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h> // from https://bitbucket.org/fmalpartida/new-liquidcrystal/wiki/Home
 
-// BEGIN SETTINGS
-#define LAYERS 19 // Total number of layers that will be moving between
-float wages[LAYERS] = { 8.25, 12.00, 15.75, 19.75, 23.50, 27.25, 31.75, 35.00, 38.75, 42.50, 46.50, 50.25, 54.00, 58.00, 61.75, 65.50, 69.25, 73.25 , 77 }; //Display values for hourly income
-String areas[LAYERS] = { "5.36", "11.10", "17.88", "29.38", "46.00", "64.80", "92.09", "126.37", "145.59", "171.57", "191.16", "217.93", "226.10", "238.64", "254.13", "259.22", "264.43", "271.85", "285.10" }; // Display values for area
+//bool debug = true;      // Turn Debug mode on  (not currently used)
 
-bool debug = true;      // Turn Debug mode on  (not currently used)
-bool stepping = true;
-int stepDelay = 1000; // milliseconds to stop each time
-// END SETTINGS
+// LEVELS
+#define levelCount 19 // Total number of levelCount that will be moving between
+float wages[levelCount] = { 8.25, 12.00, 15.75, 19.75, 23.50, 27.25, 31.75, 35.00, 38.75, 42.50, 46.50, 50.25, 54.00, 58.00, 61.75, 65.50, 69.25, 73.25 , 77 }; //Display values for hourly income
+String areas[levelCount] = { "5.36", "11.10", "17.88", "29.38", "46.00", "64.80", "92.09", "126.37", "145.59", "171.57", "191.16", "217.93", "226.10", "238.64", "254.13", "259.22", "264.43", "271.85", "285.10" }; // Display values for area
 
-// LCD SETUP
+int level = 0;
+int previous_level = 0;
+
+// LCD
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7); // SDA to A4, SDL to A5
 #define BACKLIGHT_PIN     3                       // LCD Backlight pin (on LCD, do no change)
 
-// PINS 
+// PINS
 const int buttonPin = 7;  // Button Pin - Connect between this and +5V, Pull down resistor between this and GND.
 
 const int MS1pin = 12;    // 00 is 1, 10 is 1/2
@@ -34,6 +34,9 @@ const int STEPpin = 9;    // Low-to-high is step
 const int DIRpin = 10;    // 0 is CCW, 1 is CW
 
 // STEPPING
+bool stepping = true;
+int stepDelay = 1000; // milliseconds to stop each time
+
 int microstep = 16;     // Choices are full-step, half-step, quarter-step, sixteenth-step
 int spinSpeed = 27;//4;      // Desired speed in mm/s
 
@@ -49,20 +52,17 @@ const float usteps_per_mm1 = 96.2752018f; // Sixteenth stepping
 const float usteps_per_mm2 = 95.9125834f; // Sixteenth stepping - measured
 const unsigned int timer_speed = (unsigned int) ((1.f / ((float) spinSpeed * (float) microstep * 0.0625f * usteps_per_mm2)) * 3000000.f); //24V, full-step
 
-// STATE
+// STATES
 int buttonState = 0;
 int dir = 0;
 
-int layer = 0;
-int area = 0;
-int section = 0;
-int previous_layer = 0;
-float percent = 0.0;
+//float percent = 0.0;
 
 // automated version variables
-boolean autoState = false;
-int autoPin = 3;                // Pin to connect auto run switch to
-int autoVal = 1;
+//boolean autoState = false;
+//int autoPin = 3;                // Pin to connect auto run switch to
+//int autoVal = 1;
+
 int upState = true;             // Set run direction
 // print out dir and confirm which is which
 
@@ -124,30 +124,30 @@ void setup()
   Serial.begin(9600);                            // Start Serial
 
   // automatic state setup()
-  pinMode(autoPin, INPUT_PULLUP);                  // Set internal pullup on auto run pin
+  //  pinMode(autoPin, INPUT_PULLUP);                  // Set internal pullup on auto run pin
 }
 
 //Display data to LCD
 void displayData() {
   lcd.home();
 
-  if (layer >= 19) {  // Stops display but going over the correct layer and displaying garbage because it's written out of order
+  if (level >= 19) {  // Stops display but going over the correct level and displaying garbage because it's written out of order
   }
   else {
 
     lcd.setCursor(0, 0);
-    lcd.print((layer + 1));
+    lcd.print((level + 1));
     lcd.print(" ");  // formatting and whatnot to avoid extra characters
 
     lcd.setCursor(0, 1);
     lcd.print("HOURLY WAGE: $");
-    lcd.print(wages[layer]);
+    lcd.print(wages[level]);
     lcd.print(" ");  // formatting and whatnot to avoid extra characters
     //    lcd.print(" ");
 
     lcd.setCursor(0, 2);
     lcd.print("AREA: ");
-    lcd.print(areas[layer]);
+    lcd.print(areas[level]);
     lcd.print(" SQ MI ");
   }
 
@@ -157,21 +157,21 @@ void displayData() {
 
 
 void loop() {
+  //  autoVal = digitalRead(autoPin); //check to see if it's in autorun mode
+  //  if (autoVal > 0) {
+  //    autoState = false;  // turn autorun off if the switch isn't on
+  //  } else {
+  //    autoState = true;  // Turn autorun on if the switch is on
+  //  }
 
-  autoVal = digitalRead(autoPin); //check to see if it's in autorun mode
-  if (autoVal > 0) {
-    autoState = false;  // turn autorun off if the switch isn't on
-  } else {
-    autoState = true;  // Turn autorun on if the switch is on
-  }
+  //  targetStep = floor(percent*maxStep);    // Get total steps per level
+  targetStep = floor(maxStep);    // Get total steps per level
 
-  targetStep = floor(maxStep);    // Get total steps per layer
-
-  layer = LAYERS * currentStep / targetStep;// Math for layer height
+  level = levelCount * currentStep / targetStep;// Math for level height
 
   //Printing to serial for debug purpuses
-  Serial.print("layer: ");
-  Serial.print(layer);
+  Serial.print("level: ");
+  Serial.print(level);
   Serial.print("   ");
   Serial.print("currentStep: ");
   Serial.print(currentStep);
@@ -180,7 +180,7 @@ void loop() {
   Serial.print(targetStep);
   Serial.println("");
 
-  if (stepping && layer != previous_layer) {
+  if (stepping && level != previous_level) {
     noInterrupts();
     TCCR1A &= ~(1 << COM1A0);  // Enable OCR1A pin
     interrupts();
@@ -188,20 +188,20 @@ void loop() {
   } else {
 
     // Auto mode movement
-    if (autoState) {
-      if (currentStep <= 0 || currentStep >= targetStep) {
-        upState = !upState;
-      }
-      if (upState) {
-        buttonState = 1;
-      } else {
-        buttonState = 0;
-      }
-
-      // Manual (button) mode movement
-    } else {
-      buttonState = digitalRead(buttonPin); //Check state of button
-    }
+    //    if (autoState) {
+    //      if (currentStep <= 0 || currentStep >= targetStep) {
+    //        upState = !upState;
+    //      }
+    //      if (upState) {
+    //        buttonState = 1;
+    //      } else {
+    //        buttonState = 0;
+    //      }
+    //
+    //      // Manual (button) mode movement
+    //    } else {
+    buttonState = digitalRead(buttonPin); //Check state of button
+    //    }
 
     dir = (2 * buttonState) - 1;
     displayData();
@@ -215,18 +215,18 @@ void loop() {
       interrupts();
 
       currentStep = constrain(currentStep + dir, 0, targetStep);
-      if (debug) {
-        lcd.setCursor(0, 3);
-        lcd.print("      ");
-        lcd.print("  ");
-      }
+      //      if (debug) {
+      //        lcd.setCursor(0, 3);
+      //        lcd.print("      ");
+      //        lcd.print("  ");
+      //      }
     } else {
       noInterrupts();
       TCCR1A &= ~(1 << COM1A0);  // Disable OCR1A pin
       interrupts();
     }
   }
-  previous_layer = layer;
+  previous_level = level;
 }
 
 
